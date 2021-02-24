648306932b3139c55c5a46f811bb863115272323
dali <dalissj1@gmail.com>
2021-02-24 18:23:12 +0100

    scroll animation

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+//selecting elements from DOM
 const myLis = document.querySelectorAll('nav ul li');
 const myCircles = document.querySelectorAll('nav .circle');
+const pageTable = [
+    document.querySelector('.myMap'),
+    document.querySelector('.contactForm'),
+    document.querySelector('.aboutUs')];
+const [myMap,contactForm,aboutUs]=pageTable;
 
-
+//adding nav animation
 for(let i =0; i<myLis.length; i++){
     if(!myLis[i].classList.contains("current")){
         myLis[i].addEventListener('mouseover',()=>{
@@ -16,3 +22,18 @@ for(let i =0; i<myLis.length; i++){
     }
 }
 
+//adding scroll animation
+for(let i=0; i<pageTable.length; i++){
+    myLis[i].addEventListener('click',
+        function(){
+            console.log(pageTable[i].getBoundingClientRect())
+            window.scrollTo({
+                top: window.innerHeight*i,
+                behavior:"smooth"
+            })
+            myLis.forEach(element=>{
+                element.classList.remove("current");
+            });
+            myLis[i].classList.add("current")
+        })
+}
