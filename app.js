3a8dccb794a4bf8becbdeabda60bde5fde45e86f
dali <dalissj1@gmail.com>
2021-02-26 18:59:55 +0100

    completing the navBar

--- app.js
+++ app.js
@@ -3,8 +3,31 @@ const myLis = document.querySelectorAll('nav ul li');
 const myCircles = document.querySelectorAll('nav .circle');
 const pageTable = [
     document.querySelector('.myMap'),
-    document.querySelector('#contact')];
-const [myMap,contactForm]=pageTable;
+    document.querySelector('#contact'),
+    document.querySelector('.aboutUs')    
+];
+const [myMap,contactForm, aboutUs]=pageTable;
+
+
+//deciding the the checks per second 
+
+const framePassed= function(callback){
+    window.setTimeout(callback,1000/60);
+}
+
+
+// function change current
+
+const changeCurrent = (i)=> {
+    myLis.forEach((element,index)=>{
+        element.classList.remove("current");
+        myCircles[index].classList.remove("current");
+    });
+    myLis[i].classList.add("current")
+    myCircles[i].classList.add("current");
+}
+
+
 
 //adding nav animation
 for(let i =0; i<myLis.length; i++){
@@ -16,9 +39,7 @@ for(let i =0; i<myLis.length; i++){
             myCircles[i].style.transform = "none"
         })
     }
-    else {
-        myCircles[i].style.transform = "scale(1.5)"
-    }
+    
 }
 
 //adding scroll animation
@@ -29,9 +50,32 @@ for(let i=0; i<pageTable.length; i++){
                 top: window.innerHeight*i,
                 behavior:"smooth"
             })
-            myLis.forEach(element=>{
-                element.classList.remove("current");
-            });
-            myLis[i].classList.add("current")
+            changeCurrent(i);
         })
 }
+
+//infinite loop function
+
+function settingNavBar(){
+    const [myMapPosition,contactFormPosition,aboutUsPosition] = 
+        pageTable.map(element=> element.getBoundingClientRect());
+    
+    console.log(aboutUsPosition.top +" "+window.innerWidth/2);
+    
+    if(aboutUsPosition.top<=window.innerHeight/2){
+
+        
+        changeCurrent(2);
+    }
+    else if(contactFormPosition.top<=window.innerHeight/2){
+        changeCurrent(1);
+    }
+    else{
+        changeCurrent(0);
+    }
+    
+
+    framePassed(settingNavBar);
+}
+settingNavBar();
+
