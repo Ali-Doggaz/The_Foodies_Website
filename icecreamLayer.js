652beff70efba886c551909c92d20da77407641c
nadineboukadida <78963245+nadineboukadida@users.noreply.github.com>
2021-06-11 12:11:00 +0100

    transition between slides

--- icecreamLayer.js
+++ icecreamLayer.js
@@ -1,5 +1,39 @@
 const button = document.querySelector(".buttons");
-button.addEventListener("click", ())
+let sweets = false;
+
+console.log(button);
+const firstinput = document.querySelector("#trigger1");
+console.log(firstinput);
+firstinput.onchange=() => {
+    console.log(firstinput);
+    if (firstinput.checked===true){
+        console.log("checked");
+        document.body.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+        });
+    }
+
+}
+
+button.addEventListener("click", () => {
+
+    console.log(document.body.scrollTop);
+    sweets = !document.body.scrollTop;
+    const scrolling = sweets ? window.innerHeight : 0;
+    const myclass = sweets ? "sweets" : "spicy";
+    document.body.scrollTo({
+        top: scrolling,
+        left: 0,
+        behavior: "smooth"
+    });
+
+    const myCheck = document.querySelector(`.${myclass} > .slides > input:last-of-type `)
+    console.log(myCheck);
+    myCheck.checked = true;
+
+})
 
 
 
--- modal.js
+++ modal.js
@@ -1,45 +1,44 @@
+// // Get the button that opens the modal
+// var tbtn = document.querySelectorAll("#myBtn");
 
-// Get the button that opens the modal
-var tbtn = document.querySelectorAll("#myBtn");
-
-// Get the <span> element that closes the modal
-var tspan = document.querySelectorAll(".close");
-
+// // Get the <span> element that closes the modal
+// var tspan = document.querySelectorAll(".close");
 
 
-// When the user clicks on the button, open the modal
-for (var i = 0; i < tbtn.length; i++) {
-  tbtn[i].onclick = function(e) {
-     e.preventDefault();
-     modal = document.getElementById(e.target.getAttribute("href"));
-     
-     modal.style.display = "block";
-  }
- }
-// btn.onclick = function() {
-//   modal.style.display = "block";
+
+// // When the user clicks on the button, open the modal
+// for (var i = 0; i < tbtn.length; i++) {
+//   tbtn[i].onclick = function(e) {
+//      e.preventDefault();
+//      modal = document.getElementById(e.target.getAttribute("href"));
+
+//      modal.style.display = "block";
+//   }
+//  }
+// // btn.onclick = function() {
+// //   modal.style.display = "block";
+// // }
+
+// // When the user clicks on <span> (x), close the modal
+// // for(var i=0;i<tspan.length;i++)
+// // {
+//   for (var i = 0; i < tspan.length; i++) {
+//   tspan[i].onclick=function(){
+//     var tmodal=document.querySelectorAll(".modal");
+//     for(var index in tmodal)
+//     {
+//       if (typeof tmodal[index].style !== 'undefined') tmodal[index].style.display = "none";
+//     }}
+
+//   }
+
+//  // for(var j=0;j<tmodal.length;j++)
+//     // {console.log(tmodal[j]);
+//     //   tmodal[j].style.display = "none";}
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
 // }
-
-// When the user clicks on <span> (x), close the modal
-// for(var i=0;i<tspan.length;i++)
-// {
-  for (var i = 0; i < tspan.length; i++) {
-  tspan[i].onclick=function(){
-    var tmodal=document.querySelectorAll(".modal");
-    for(var index in tmodal)
-    {
-      if (typeof tmodal[index].style !== 'undefined') tmodal[index].style.display = "none";
-    }}
-   
-  }
-
- // for(var j=0;j<tmodal.length;j++)
-    // {console.log(tmodal[j]);
-    //   tmodal[j].style.display = "none";}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
