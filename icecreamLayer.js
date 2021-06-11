196395908e07c873f3659f1045f32a60144e0488
nadineboukadida <78963245+nadineboukadida@users.noreply.github.com>
2021-06-11 10:48:14 +0100

    second slide

--- icecreamLayer.js
+++ icecreamLayer.js
@@ -1,13 +1,20 @@
-const buttons = document.getElementsByClassName("buttons");
+const button = document.querySelector(".buttons");
+button.addEventListener("click", ())
 
-for (const button of buttons) {
-  button.addEventListener('click', () => {
-     var id = button.getAttribute("id");
-    
-     var layerClass = ".top-layer";
-     var layers = document.querySelectorAll(layerClass);
-     for (const layer of layers) {
-       layer.classList.toggle("active");
-     }
-  });
-}
+
+
+
+
+
+
+// for (const button of buttons) {
+//   button.addEventListener('click', () => {
+//      var id = button.getAttribute("id");
+
+//      var layerClass = ".top-layer";
+//      var layers = document.querySelectorAll(layerClass);
+//      for (const layer of layers) {
+//        layer.classList.toggle("active");
+//      }
+//   });
+// }
